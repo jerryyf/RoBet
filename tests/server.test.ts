5b875c5d097b8f43c2132cfd5ff8ba5ec1bd3f09
jerryyf <jerryyf@pm.me>
2023-07-26 11:26:51 +1000

    Fix message

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -5,7 +5,7 @@ describe('testing SC deployment', () => {
     const fn = jest.fn(await startGameBetWrapper("0x927DBCFc80f7Bae8f9D0Db608EA5f628737A0511", "0x7b452A989E57681699a1CC520E23572a423EEEF6", 1, 1));
     expect(fn).toBeTruthy();
   });
-  test('startGameBetWrapper should not throw errors', async () => {
+  test('playerChoice should work', async () => {
     const ret = await startGameBetWrapper("0x927DBCFc80f7Bae8f9D0Db608EA5f628737A0511", "0x7b452A989E57681699a1CC520E23572a423EEEF6", 1, 1);
     const fn = jest.fn(await playerChoice(ret, 1, 1));
     expect(fn).toBeTruthy();
